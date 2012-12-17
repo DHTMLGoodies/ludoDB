e4f2ec0b5af7634c8c7e010f32fc37dd456a4e14
DHTMLGoodies <post@dhtmlgoodies.com>
2012-12-17 18:22:59 +0100

    New methods for test class Person

--- Tests/Person.php
+++ Tests/Person.php
@@ -28,8 +28,24 @@ class Person extends LudoDbTable
         $this->setValue('firstname', $value);
     }
 
+    public function setLastName($value){
+        $this->setvalue('lastname', $value);
+    }
+
     public function setZip($value){
         $this->setValue('zip', $value);
+    }
+
+    public function getFirstname(){
+        return $this->getvalue('firstname');
+    }
+
+    public function getLastname(){
+        return $this->getvalue('lastname');
+    }
+
+    public function getZip(){
+        return $this->getValue('zip');
     }
 
     public function getCity(){
