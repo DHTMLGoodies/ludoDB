6d52f6cc74532e309bd7b9ae4ccb2f5900a6a0b2
DHTMLGoodies <post@dhtmlgoodies.com>
2012-12-18 01:05:19 +0100

    Support for default data

---
+++ Tests/Car.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: borrow
+ * Date: 18.12.12
+ * Time: 00:52
+ * To change this template use File | Settings | File Templates.
+ */
+class Car
+{
+
+}
