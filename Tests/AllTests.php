4d00c6ca83545e321e14829a829b1c804b76fa6e
DHTMLGoodies <post@dhtmlgoodies.com>
2012-12-19 15:40:37 +0100

    Base class for tests

---
+++ Tests/AllTests.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: borrow
+ * Date: 19.12.12
+ * Time: 15:32
+ * To change this template use File | Settings | File Templates.
+ */
+class AllTests
+{
+
+}
---
+++ Tests/TestBase.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: borrow
+ * Date: 19.12.12
+ * Time: 15:37
+ * To change this template use File | Settings | File Templates.
+ */
+class TestBase
+{
+
+}
