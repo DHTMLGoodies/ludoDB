874b66222590d38745254d72c2987735510efa9b
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-03 01:44:51 +0100

    Checking for sub class of LudoDBModel/Collection in request handler

--- LudoDBRequestHandler.php
+++ LudoDBRequestHandler.php
@@ -121,7 +121,7 @@ class LudoDBRequestHandler
 
     private function getReflectionClass($className){
         $cl = new ReflectionClass($className);
-        if (!$cl->isSubclassOf('LudoDBModel') || $cl->isAbstract() || $cl->isInterface()) {
+        if (!$cl->isSubclassOf('LudoDBObject') || $cl->isAbstract() || $cl->isInterface()) {
             throw new LudoDBClassNotFoundException('Invalid request for: ' . $className, 400);
         }
         return $cl;
