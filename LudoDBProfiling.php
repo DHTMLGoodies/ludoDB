e1bc4ec296d2efcd627b810370e7bee391fc7a4d
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-18 21:34:03 +0100

    Fixed returned path from profiler

--- LudoDBProfiling.php
+++ LudoDBProfiling.php
@@ -18,53 +18,83 @@ class LudoDBProfiling implements LudoDBService
     private $name;
     private $start;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->arguments = func_get_args();
     }
-    public function getValidServices(){
+
+    public function getValidServices()
+    {
         return array("profile");
     }
 
-    public function validateArguments($service, $arguments){
+    public function validateArguments($service, $arguments)
+    {
         return count($arguments) >= 2;
     }
 
-    public function profile(){
+    public function profile($data = array())
+    {
 
         $request = implode("/", $this->arguments);
         $this->start(preg_replace("/[^0-9a-z]/si", "", $request));
 
         $handler = new LudoDBRequestHandler();
-        $handler->handle($request);
+        $handler->handle(array(
+            'request' => $request,
+            'data' => $data
+        ));
 
         return $this->end();
     }
 
-    public function validateServiceData($service, $data){
+    public function validateServiceData($service, $data)
+    {
         return true;
     }
-    public function shouldCache($service){
+
+    public function shouldCache($service)
+    {
         return false;
     }
 
-    public function getOnSuccessMessageFor($service){
+    public function getOnSuccessMessageFor($service)
+    {
         return "";
     }
 
-    public function start($name){
+    public function start($name)
+    {
         $this->start = microtime(true);
         $this->name = $name;
-        xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+        if (!function_exists("xhprof_enable")) {
+            throw new LudoDBException("xhprof is not enabled on your server");
+        }
+        $flags = defined("XHPROF_FLAGS_CPU") ? XHPROF_FLAGS_CPU : 0;
+        $flags += defined("XHPROF_FLAGS_MEMORY") ? XHPROF_FLAGS_MEMORY : 0;
+        xhprof_enable($flags);
     }
 
-    public function getTimeUsage(){
+    public function getTimeUsage()
+    {
         return microtime(true) - $this->start;
     }
 
-    public function end(){
-        $profilingData = xhprof_disable();
-        $profilingRuns = new XHProfRuns_Default();
-        $run_id = $profilingRuns->save_run($profilingData, $this->name);
-        return "http://localhost:8080/xhprof/xhprof_html/index.php?run=$run_id&source=". $this->name;
+    public function end()
+    {
+        if (function_exists("xhprof_disable")) {
+            $profilingData = xhprof_disable();
+            $profilingRuns = new XHProfRuns_Default();
+            $run_id = $profilingRuns->save_run($profilingData, $this->name);
+            return "http://" . $_SERVER['HTTP_HOST']. "/" . $this->getPath()."/xhprof/xhprof_html/index.php?run=$run_id&source=" . $this->name;
+        }
+        return null;
+    }
+
+    private function getPath(){
+        $path = explode(DIRECTORY_SEPARATOR, dirname(__FILE__));
+        $countTokens = count(explode("/", $_SERVER['DOCUMENT_ROOT']));
+        $path = array_slice($path, $countTokens);
+        return implode("/", $path);
     }
 }
